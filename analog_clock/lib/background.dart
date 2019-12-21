0c7e7ef6c76920bfefa4606c040e0da761557837
Cinis Roman <52065414+tsinis@users.noreply.github.com>
2019-12-21 19:01:30 +0100

    Changed animation Controller

--- analog_clock/lib/background.dart
+++ analog_clock/lib/background.dart
@@ -3,9 +3,6 @@ import 'package:flare_flutter/flare_actor.dart';
 import 'package:flare_flutter/flare_controls.dart';
 import 'package:flutter/widgets.dart';
 
-const String _timeAnimationName = 'min_sec';
-const String _dayNightAnimationName = 'hour_day';
-
 class RiveAnimation extends StatefulWidget {
   @override
   _RiveAnimationState createState() => _RiveAnimationState();
@@ -22,24 +19,20 @@ class _RiveAnimationState extends State<RiveAnimation> {
 }
 
 class _AnimationController extends FlareControls {
-  static ActorAnimation _dayNightAnimation;
   static ActorAnimation _timeAnimation;
 
   @override
   void initialize(FlutterActorArtboard artboard) {
     super.initialize(artboard);
-    play('thermometer');
-    _dayNightAnimation = artboard.getAnimation(_dayNightAnimationName);
-    _timeAnimation = artboard.getAnimation(_timeAnimationName);
-    play(_dayNightAnimationName);
-    play(_timeAnimationName);
+    _timeAnimation = artboard.getAnimation('time');
+    // play('thermometer');
   }
 
   @override
   bool advance(FlutterActorArtboard artboard, double elapsed) {
     final DateTime _now = DateTime.now();
-    _timeAnimation.apply(_now.second / 60 + _now.minute, artboard, 1.0);
-    _dayNightAnimation.apply(_now.minute / 60 + _now.hour, artboard, 1.0);
+    _timeAnimation.apply(
+        _now.second / 60 + _now.minute + _now.hour * 60, artboard, 1.0);
     return true;
   }
 }
