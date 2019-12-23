78d47857068492a0209c8be50fe9fdbdcb4c9305
Cinis Roman <52065414+tsinis@users.noreply.github.com>
2019-12-23 09:04:01 +0100

    Testing Cuckoo and Antia-Alias disable

--- analog_clock/lib/background.dart
+++ analog_clock/lib/background.dart
@@ -20,19 +20,23 @@ class _RiveAnimationState extends State<RiveAnimation> {
 
 class _AnimationController extends FlareControls {
   static ActorAnimation _timeAnimation;
+  ActorAnimation _cuckooAnimation;
 
   @override
   void initialize(FlutterActorArtboard artboard) {
     super.initialize(artboard);
     _timeAnimation = artboard.getAnimation('time');
-    // play('thermometer');
+    _cuckooAnimation = artboard.getAnimation('cuckoo');
   }
 
   @override
   bool advance(FlutterActorArtboard artboard, double elapsed) {
     final DateTime _now = DateTime.now();
-    _timeAnimation.apply(
-        _now.second / 60 + _now.minute + _now.hour * 60, artboard, 1.0);
+    (_now.minute == 0)
+        ? _cuckooAnimation.apply(
+            _now.millisecond / 1000 + _now.second, artboard, 1.0)
+        : _timeAnimation.apply(
+            _now.second / 60 + _now.minute + _now.hour * 60, artboard, 1.0);
     return true;
   }
 }
