888bd44ec0d010f97b82bcb42543019533cdbaf4
Cinis Roman <52065414+tsinis@users.noreply.github.com>
2020-01-02 05:36:46 +0100

    Added temperature converter module

--- analog_clock/lib/temperature_converter.dart
+++ analog_clock/lib/temperature_converter.dart
@@ -1,3 +1,18 @@
 double mercuryPosition(String _temperatureFromHelper) {
-  return 0.0;
+  String _celsiusWOUnit;
+  double _celsius;
+
+  if (_temperatureFromHelper.endsWith('F')) {
+    _celsiusWOUnit = _removeDegreesUnit(_temperatureFromHelper);
+    _celsius = (double.parse(_celsiusWOUnit) - 32) * 5 / 9;
+  } else {
+    _celsiusWOUnit = _removeDegreesUnit(_temperatureFromHelper);
+    _celsius = double.parse(_celsiusWOUnit);
+  }
+  _celsius = (_celsius - 60) * -2.35;
+  return _celsius;
+}
+
+String _removeDegreesUnit(String _temperatureFromHelper) {
+  return _temperatureFromHelper.substring(0, _temperatureFromHelper.length - 2);
 }
